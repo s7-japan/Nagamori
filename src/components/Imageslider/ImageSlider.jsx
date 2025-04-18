1f03bcaefc0bd583d552921dcccea3dd0091a19c
= <=>
2025-04-18 20:47:50 +0530

    Fixed the image banner issue

--- src/components/Imageslider/ImageSlider.jsx
+++ src/components/Imageslider/ImageSlider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ImageSlider.css";
 import sliderone from "../../assets/slider1.png";
 import slidertwo from "../../assets/slider2.png";
@@ -6,33 +6,49 @@ import sliderthree from "../../assets/slider3.png";
 import sliderfour from "../../assets/slider4.png";
 
 const ImageSlider = () => {
+  const [animate, setAnimate] = useState(false);
+
+  // Preload images
+  useEffect(() => {
+    const images = [sliderone, slidertwo, sliderthree, sliderfour];
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+    });
+
+    // Delay animation to ensure images are loaded
+    const timer = setTimeout(() => {
+      setAnimate(true);
+    }, 100); // 100ms delay
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <>
-      <div id="main">
-        <div className="moving md:mt-0">
-          <div className="left"></div>
-          <div className="move">
-            <img src={sliderone} alt="" className="h-25 md:h-[9vw]" />
-            <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderone} alt="" className="h-25 md:h-[9vw]" />
-            <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderone} alt="" className="h-25 md:h-[9vw]" />
-            <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderone} alt="" className="h-25 md:h-[9vw]" />
-            <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" />
-            <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" />
-          </div>
-          <div className="right"></div>
+    <div id="main">
+      <div className="moving md:mt-0">
+        <div className="left"></div>
+        <div className={`move ${animate ? "animate" : ""}`}>
+          <img src={sliderone} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderone} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderone} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderone} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={slidertwo} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderthree} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
+          <img src={sliderfour} alt="" className="h-25 md:h-[9vw]" loading="lazy" />
         </div>
+        <div className="right"></div>
       </div>
-    </>
+    </div>
   );
 };
 
