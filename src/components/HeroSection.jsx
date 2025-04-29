66f479346247ed086182c07161ac82289b647277
Torikul <devtorikul@gmail.com>
2025-04-29 15:27:20 +0600

    completed 1 to 7

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ export default function HeroSection() {
         <img
           src={whitelogo}
           alt=""
-          className="absolute w-[300px] top-5 left-1/2 transform -translate-x-1/2"
+          className="absolute w-[300px] top-7 left-1/2 transform -translate-x-1/2"
         />
         <img
           src={makeupLine}
--- src/components/Menu2.jsx
+++ src/components/Menu2.jsx
@@ -22,25 +22,23 @@ const Menu2 = () => {
       <p className="text-center mb-24 md:mb-[2.2vw] text-[7vw] md:text-[2.8vw] mt-14 md:mt-[4.5vw] text-[#707070] font-[400]">
         4つの特徴と強み
       </p>
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-x-4 gap-y-24 md:gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-20 md:gap-8">
         <div className="flex flex-col">
-          <div className="">
+          <div>
             <img src={menu2_1} alt="" className="w-full" />
           </div>
-          <div className="relative">
-            <img
-              src={tail}
-              alt=""
-              className="md:h-[2.5vw] absolute top-[8vw] md:top-[1.3vw]"
-            />
-            <p className="text-[4vw] md:text-[0.9vw] text-[#707070] font-[500] mt-[17.5vw] md:mb-0 mb-[8vw] md:mt-[3.2vw]">
+          <div>
+            <img src={tail} alt="" className="mt-9" />
+            <p className="text-xl mt-1 md:-mt-4 leading-10 text-[#606060] font-medium ">
               テールアイライン
             </p>
-            <p className="text-[4vw] md:text-[0.75vw] leading-8 md:leading-[1.6vw] md:mt-[0.8vw] text-[#707070] font-[500]">
+            <p className="text-black leading-9 mt-[30px]">
               「きれいなテールライン」をつくります。
-              アートメイクで、美しいテールラインを入れることで、横幅のひろがりをつくり、目を大きく見せることができます。
-              <br /> また、お化粧に時間をかける必要もなくなります。
+              <br />
+              アートメイクで、美しいテールラインを入れることで、横幅のひろがりをつくり、目を大きく見せることができます。また、お化粧に時間をかける必要もなくなります。
+              <br />
               この症例は、ドットの集合体により、メイクで描くよりも美しいアイラインを実現しています。
+              <br />
               技法として難易度が高いことから、「きれいなテールライン」を入れられるアーティストはとても少なく、全国からお客様にご来院いただいております。
             </p>
           </div>
@@ -50,16 +48,12 @@ const Menu2 = () => {
           <div>
             <img src={menu2_2} alt="" className="w-full" />
           </div>
-          <div className="relative">
-            <img
-              src={shadow}
-              alt=""
-              className="md:h-[2.5vw] absolute top-[8vw] md:top-[1.3vw]"
-            />
-            <p className="text-[4vw] md:text-[0.9vw] text-[#707070] font-[500] mt-[17.5vw] md:mb-0 mb-[8vw] md:mt-[3.2vw]">
+          <div>
+            <img src={shadow} alt="" className="mt-9" />
+            <p className="text-xl mt-1 md:-mt-4 leading-10 text-[#606060] font-medium ">
               シャドウアイライン
             </p>
-            <p className="text-[4vw] md:text-[0.75vw] leading-8 md:leading-[1.6vw] md:mt-[0.8vw] text-[#707070] font-[500]">
+            <p className="text-black leading-9 mt-[30px]">
               「シャドウアイラインで、魅力的な目もとにしています。
               <br />
               二重の幅や骨格に合わせてシャドウの入れる幅をかえることで、日本人に間に合うシャドウラインを実現しました。
@@ -75,16 +69,12 @@ const Menu2 = () => {
           <div>
             <img src={menu2_3} alt="" className="w-full" />
           </div>
-          <div className="relative">
-            <img
-              src={lips}
-              alt=""
-              className="md:h-[2.5vw] absolute top-[8vw] md:top-[1.3vw]"
-            />
-            <p className="text-[4vw] md:text-[0.9vw] text-[#707070] font-[500] mt-[17.5vw] md:mb-0 mb-[8vw] md:mt-[3.2vw]">
+          <div>
+            <img src={lips} alt="" className="mt-9" />
+            <p className="text-xl mt-1 md:-mt-4 leading-10 text-[#606060] font-medium ">
               リップ
             </p>
-            <p className="text-[4vw] md:text-[0.75vw] leading-8 md:leading-[1.6vw] md:mt-[0.8vw] text-[#707070] font-[500]">
+            <p className="text-black leading-9 mt-[30px]">
               「美しい定着」により、口紅を塗ったような仕上がりとなります。薄くなりやすい内側は濃くしっかりと、外側は淡く優しくグラデーションで重ねます。
               <br />
               技法のバリエーションには自信があり、いくつかの技法を組み合わせることで、綺麗に発色しつつも、透明感のある仕上がりにします。
@@ -100,16 +90,12 @@ const Menu2 = () => {
           <div>
             <img src={menu2_4} alt="" className="w-full" />
           </div>
-          <div className="relative">
-            <img
-              src={hairline}
-              alt=""
-              className="md:h-[2.5vw] absolute top-[8vw] md:top-[1.3vw]"
-            />
-            <p className="text-[4vw] md:text-[0.9vw] text-[#707070] font-[500] mt-[17.5vw] md:mb-0 mb-[8vw] md:mt-[3.2vw]">
+          <div>
+            <img src={hairline} alt="" className="mt-9" />
+            <p className="text-xl mt-1 md:-mt-4 leading-10 text-[#606060] font-medium ">
               ヘアライン
             </p>
-            <p className="text-[4vw] md:text-[0.75vw] leading-8 md:leading-[1.6vw] md:mt-[0.8vw] text-[#707070] font-[500]">
+            <p className="text-black leading-9 mt-[30px]">
               自然なヘアラインを仕上げます。男性、女性ともにとても人気のメニューです。
               <br />
               マシンによるドットの集合体により、細くて繊細な毛並みを描くことで、本物のような自然なヘアラインにします。ひたいを丸く見せたり、おもながに見えることを気にしている方には、お顔のたて幅を和らげたり、また、M字の薄い部分を濃くしたり、ご要望に合わせたデザインが可能です。
